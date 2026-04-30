ddc2f30f26c3c164f86f899be7fe619c00c14024
João Henrique <jhenrique.caval@hotmail.com>
2026-04-30 14:03:14 -0300

    feat: estruturação inicial do projeto

---
+++ src/app/features/pacientes/paciente-form/paciente-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paciente-form',
+  imports: [],
+  templateUrl: './paciente-form.html',
+  styleUrl: './paciente-form.css',
+})
+export class PacienteForm {
+
+}
---
+++ src/app/features/pacientes/paciente-list/paciente-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paciente-list',
+  imports: [],
+  templateUrl: './paciente-list.html',
+  styleUrl: './paciente-list.css',
+})
+export class PacienteList {
+
+}
