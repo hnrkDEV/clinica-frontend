a58615fae0452358473da5d4a0dec040f01128f1
João Henrique <jhenrique.caval@hotmail.com>
2026-04-30 23:20:53 -0300

    feat: criação das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,51 @@
 import { Routes } from '@angular/router';
+import { AgendamentoForm } from './features/agendamentos/agendamento-form/agendamento-form';
+import { AgendamentoList } from './features/agendamentos/agendamento-list/agendamento-list';
+import { AuditoriaList } from './features/auditoria/auditoria-list/auditoria-list';
+import { PacienteForm } from './features/pacientes/paciente-form/paciente-form';
+import { PacienteList } from './features/pacientes/paciente-list/paciente-list';
+import { ProfissionalForm } from './features/profissionais/profissional-form/profissional-form';
+import { ProfissionalList } from './features/profissionais/profissional-list/profissional-list';
+import { MainLayout } from './layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'pacientes',
+        pathMatch: 'full',
+      },
+      {
+        path: 'pacientes',
+        component: PacienteList,
+      },
+      {
+        path: 'pacientes/novo',
+        component: PacienteForm,
+      },
+      {
+        path: 'profissionais',
+        component: ProfissionalList,
+      },
+      {
+        path: 'profissionais/novo',
+        component: ProfissionalForm,
+      },
+      {
+        path: 'agendamentos',
+        component: AgendamentoList,
+      },
+      {
+        path: 'agendamentos/novo',
+        component: AgendamentoForm,
+      },
+      {
+        path: 'auditoria',
+        component: AuditoriaList,
+      },
+    ],
+  },
+];
