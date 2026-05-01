7bfbfa83c8a97f55d64ae32707c0546b566edb25
João Henrique <jhenrique.caval@hotmail.com>
2026-04-30 23:23:58 -0300

    feat: formulário de paciente feito

--- src/app/features/pacientes/paciente-form/paciente-form.ts
+++ src/app/features/pacientes/paciente-form/paciente-form.ts
@@ -1,11 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { PacienteService } from '../../../core/services/paciente';
+import { Paciente } from '../../../shared/models/paciente';
 
 @Component({
   selector: 'app-paciente-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './paciente-form.html',
-  styleUrl: './paciente-form.css',
 })
 export class PacienteForm {
+  paciente: Paciente = {
+    nome: '',
+    email: '',
+    telefone: '',
+  };
 
+  salvando = false;
+  erro = '';
+
+  constructor(private pacienteService: PacienteService, private router: Router) {}
+
+  salvar(): void {
+    this.erro = '';
+
+    if (!this.paciente.nome || !this.paciente.email || !this.paciente.telefone) {
+      this.erro = 'Preencha todos os campos.';
+      return;
+    }
+
+    this.salvando = true;
+
+    this.pacienteService.cadastrar(this.paciente).subscribe({
+      next: () => {
+        this.salvando = false;
+        this.router.navigate(['/pacientes']);
+      },
+      error: () => {
+        this.erro = 'Erro ao cadastrar paciente.';
+        this.salvando = false;
+      },
+    });
+  }
 }
